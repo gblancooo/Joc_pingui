75c46d4ecbc547a600e8576b57123deb4bfc383a
gblancooo <gerardblanco2006@gmail.com>
2025-05-15 18:39:12 +0200

    tus putos muertos

--- joc_pingui/src/modelo/Tablero.java
+++ joc_pingui/src/modelo/Tablero.java
@@ -1,42 +1,63 @@
 package modelo;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class Tablero {
+
     public static final int NUM_CASILLAS = 50;
-    private Casilla[] casillas;
+
+    private List<Casilla> casillas;
 
     public Tablero() {
-        casillas = new Casilla[NUM_CASILLAS];
+        casillas = new ArrayList<>();
         generarCasillasAleatorias();
     }
 
     private void generarCasillasAleatorias() {
-        List<Integer> pos = new ArrayList<>();
-        for (int i=0;i<NUM_CASILLAS;i++) pos.add(i);
-        Collections.shuffle(pos);
+        Random rnd = new Random();
 
-        for(int i=0;i<5;i++) casillas[pos.remove(0)] = new Oso(pos.get(0));
-        for(int i=0;i<5;i++) casillas[pos.remove(0)] = new Agujero(pos.get(0));
-        for(int i=0;i<5;i++) casillas[pos.remove(0)] = new Trineo(pos.get(0));
-        for(int i=0;i<5;i++) casillas[pos.remove(0)] = new Interrogant(pos.get(0));
-        for(int i=0;i<NUM_CASILLAS;i++)
-            if(casillas[i]==null) casillas[i] = new CasillaNormal(i);
+        // Inicialment totes NORMAL
+        for (int i = 0; i < NUM_CASILLAS; i++) {
+            casillas.add(new Casilla(i, "NORMAL"));
+        }
+
+        // Fixem límits per tipus especials
+        int numOsos = 5;
+        int numAgujeros = 5;
+        int numTrineos = 4;
+        int numInterrogants = 6;
+
+        // Assignem tipus especials en posicions aleatòries
+        assignarTipusAleatori("OSO", numOsos, rnd);
+        assignarTipusAleatori("AGUJERO", numAgujeros, rnd);
+        assignarTipusAleatori("TRINEO", numTrineos, rnd);
+        assignarTipusAleatori("INTERROGANTE", numInterrogants, rnd);
     }
 
-    public Casilla getCasilla(int posicion) {
-        if (posicion<0) return casillas[0];
-        if (posicion>=NUM_CASILLAS) return casillas[NUM_CASILLAS-1];
-        return casillas[posicion];
+    private void assignarTipusAleatori(String tipus, int quantitat, Random rnd) {
+        int assignats = 0;
+        while (assignats < quantitat) {
+            int pos = rnd.nextInt(NUM_CASILLAS);
+
+            // Evitem posició 0 (inici) i 49 (final), i evitem sobreescriure
+            Casilla actual = casillas.get(pos);
+            if (pos != 0 && pos != NUM_CASILLAS - 1 && actual.getTipo().equals("NORMAL")) {
+                casillas.set(pos, new Casilla(pos, tipus));
+                assignats++;
+            }
+        }
     }
 
-    public Casilla[] getCasillas() {
+    public Casilla getCasilla(int index) {
+        if (index < 0) index = 0;
+        if (index >= NUM_CASILLAS) index = NUM_CASILLAS - 1;
+        return casillas.get(index);
+    }
+
+    public List<Casilla> getCasillas() {
         return casillas;
-    }
-
-    public void setCasillas(Casilla[] c) {
-        if (c.length!=NUM_CASILLAS)
-            throw new IllegalArgumentException("Mida incorrecta");
-        this.casillas = c;
     }
 }
